17211b727b83d62ec96e05ead397bb1f7f579e9e
niloy1hasan <notecse23@gmail.com>
2025-09-27 22:00:31 +0600

    progress and resolved for useState added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 import Banner from './Components/Banner/Banner'
 import Navbar from './Components/Navbar/Navbar'
@@ -15,24 +15,50 @@ const loadTicketData = async() => {
 const ticketPromise = loadTicketData();
 
 function App() {
+  //progress state
+  const [progressTask, setProgressTask] = useState([]);
+  const handleProgressTask = (progress, isAdded=true) => {
+    if(isAdded){
+      setProgressTask([...progressTask, progress]);
+    } else if(!isAdded) {
+      const progressList = progressTask.filter(item => item.id!==progress.id);
+      setProgressTask(progressList);
+    }
+  }
+
+  //resolved state
+  const [resolvedList, setResolvedList] = useState([]);
+  const handleResolvedTask = (completed, isAdded=true) => {
+    if(isAdded){
+    setResolvedList([...resolvedList, completed]);
+    } else if(!isAdded){
+      const completedList = resolvedList.filter(item => item.id!==completed.id);
+      setResolvedList(completedList);
+    }
+  }
 
   return (
     <>
       <Navbar></Navbar>
       <main className='max-w-[1600px] mx-auto'>
-        <Banner></Banner>
+        <Banner progressTask={progressTask} resolvedList={resolvedList}></Banner>
+
         <section className='max-w-[1600px] w-[93%] mt-8 mx-auto flex flex-col gap-6 md:flex-row'>
+          {/* ticket section */}
           <div className='flex-1'>
-            <Suspense fallback={<span className="loading loading-infinity loading-xl"></span>}>
-            <Ticket ticketPromise={ticketPromise}></Ticket>
+            <Suspense fallback={<span className="max-w-[1600px] mx-auto my-5 mt-10 flex justify-center loading loading-infinity loading-xl"></span>}>
+            <Ticket ticketPromise={ticketPromise} handleProgressTask={handleProgressTask}></Ticket>
           </Suspense>
           </div>
+
+          {/* side bar section for task status and resolved task */}
           <div className='w-full md:w-[280px] lg:w-[350px]'>
-            <TaskStatus></TaskStatus>
-            <ResolvedTask></ResolvedTask>
+            <TaskStatus progressTask={progressTask} handleResolvedTask={handleResolvedTask} handleProgressTask={handleProgressTask}></TaskStatus>
+            <ResolvedTask resolvedList={resolvedList} handleResolvedTask={handleResolvedTask}></ResolvedTask>
           </div>
         </section>
       </main>
+      {/* footer section */}
       <Footer></Footer>
     </>
   )

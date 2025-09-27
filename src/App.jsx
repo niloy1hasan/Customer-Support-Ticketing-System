59915d8c57b7f8a2d05bf729d3ace617d35b74cd
niloy1hasan <notecse23@gmail.com>
2025-09-27 13:14:45 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, numquam?</h1>
+      <Navbar></Navbar>
     </>
   )
 }
